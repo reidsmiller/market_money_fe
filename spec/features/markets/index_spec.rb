93781c7c70d705d09513077a9a9df540df5fadb6
Reid Miller <reid.s.miller@gmail.com>
2023-05-25 13:11:17 -0600

    test: Add markets index page test

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Market index page' do
-  describe 'As a visitor when I visit /markets', :vcr do
+  describe 'As a visitor when I visit /markets' do
     before(:each) do
       visit '/markets'
     end
 
-    it 'I see all markets listed with name, city, and state' do
+    it 'I see all markets listed with name, city, and state', :vcr do
+      markets = MarketsFacade.new.markets
       table = find('table')
-      expect(table).to have_content('Name')
-      expect(table).to have_content('City')
-      expect(table).to have_content('State')
+      expect(table).to have_css('th', text: 'Name')
+      expect(table).to have_css('th', text: 'City')
+      expect(table).to have_css('th', text: 'State')
 
-      expect(table).to be_visible
-      expect(table).to have_css('tr', count: 3)
-      expect(table).to have_css('td', count: 4)
+      within "#market_#{markets.first.id}" do
+        expect(page).to have_content(markets.first.name)
+        expect(page).to have_content(markets.first.city)
+        expect(page).to have_content(markets.first.state)
+        expect(page).to have_button('More Info')
+      end
+    end
+
+    it 'I can click on a more info button to go to that market show page', :vcr do
+      markets = MarketsFacade.new.markets
+
+      within "#market_#{markets.first.id}" do
+        click_button('More Info')
+      end
+
+      expect(current_path).to eq("/markets/#{markets.first.id}")
     end
   end
 end
